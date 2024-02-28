619c19216e72b5e0bb4157ff9a631ad5455fa357
Adevnater <sriouaiphonchai.w@gmail.com>
2024-02-28 17:23:19 +0700

    addon html

--- G_test/G_test.ino
+++ G_test/G_test.ino
@@ -8,39 +8,55 @@
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 
-
-// #define ssid = "injectorhome_2.4GHz"
-// #define password = "sirapols0422"
-
-// #define LEDin 2
 // Replace with your network credentials
 const char* ssid = "injectorhome_2.4GHz";
 const char* password = "sirapols0422";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
 // Set LED GPIO
 const int LEDin = 2;
 // Stores LED state
 String ledState;
+String DEVICE;
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+String setD = "dafult"; // 0 is slave 1 is master
+String Ontime = "dafult"; // time(ms)
+String Offtime = "dafult"; // time(ms)
+String Pattern = "dafult"; // pattern LED
+const char* param01 = "setD";
+const char* param02 = "ONTIME";
+const char* param03 = "OFFTIME";
+const char* param04 = "PATTERN";
+
+
+
 
 // Replaces placeholder with LED state value
 String processor(const String& var){
   Serial.println(var);
-  if(var == "STATE"){
-    if(digitalRead(LEDin)){
-      ledState = "ON";
+  if(var == "DEVICESTATUS"){
+    if(setD == "1"){
+      DEVICE = "MASTER";
     }
-    else{
-      ledState = "OFF";
+    else if (setD == "0"){
+      DEVICE = "SLAVE";
     }
-    Serial.print(ledState);
-    return ledState;
+    Serial.println(DEVICE);
+    return DEVICE;
   }
+  if(var == "timeON")
+    return Ontime;
+  if(var == "timeOFF")
+    return Offtime;
+  if(var == "Patt")
+    return Pattern;
   return String();
 }
- 
+    
+
+
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
@@ -53,11 +69,16 @@ void setup(){
   }
 
   // Connect to Wi-Fi
+
+  // WiFi.softAP(ssid,password);
+  // WiFi.softapAPConfig(local_ip,gateway,subnet);
+  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
+  delay(500);
 
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
@@ -68,21 +89,26 @@ void setup(){
   });
   
   // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(LEDin, HIGH);
-    Serial.println("LED on");   
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(LEDin, LOW);
-    Serial.println("LED off");   
+  // server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+  //   request->send(SPIFFS, "/style.css", "text/css");
+  // });
+  server.on("/submit", HTTP_GET, [](AsyncWebServerRequest *request){
+   if(request->hasParam(param01)){
+      setD = request->getParam(param01)->value();
+      Serial.print("setD is ");Serial.println(setD);
+    }
+    if(request->hasParam(param02)){
+      Ontime = request->getParam(param02)->value();
+      Serial.print("ONTIME is ");Serial.println(Ontime);
+    }
+    if(request->hasParam(param03)){
+      Offtime = request->getParam(param03)->value();
+      Serial.print("OFFTIME is ");Serial.println(Offtime);
+    }
+    if(request->hasParam(param04)){
+      Pattern = request->getParam(param04)->value();
+      Serial.print("Pattern is ");Serial.println(Pattern);
+    }
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
 
@@ -91,5 +117,15 @@ void setup(){
 }
  
 void loop(){
-  
+  delay(1000);
+    if (setD == "0"){
+      Serial.print("Device is Slave  "); Serial.println(setD);
+    } else if (setD == "1"){
+      Serial.print("Device is Master  "); Serial.println(setD);
+    }
+    Serial.print("ontime is ");Serial.println(Ontime);
+    Serial.print("offtime is ");Serial.println(Offtime);
+    Serial.print("Pattern is ");Serial.println(Pattern);
+    Serial.println("NEWWWWWW");
+    delay(1000);
 }
