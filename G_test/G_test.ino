5951c8b4239733b1d7ef73f0ad13b69d050661a9
Adevnater <sriouaiphonchai.w@gmail.com>
2024-02-29 15:35:57 +0700

    adjust parameter and html

--- G_test/G_test.ino
+++ G_test/G_test.ino
@@ -17,43 +17,44 @@ const char* password = "12345678";
 AsyncWebServer server(80);
 // WiFiServer server(80);
 
-// Set LED GPIO
-const int LEDin = 2;
-// Stores LED state
-String ledState;
-String DEVICE;
-
-String setD = "dafult"; // 0 is slave 1 is master
+String SendMac = "dafult";
+String ReceiveMac = "dafult";
 String Ontime = "dafult"; // time(ms)
 String Offtime = "dafult"; // time(ms)
 String Pattern = "dafult"; // pattern LED
-const char* param01 = "setD";
-const char* param02 = "ONTIME";
-const char* param03 = "OFFTIME";
-const char* param04 = "PATTERN";
+String DeviceMode = "dafult"; // 0 is slave 1 is master
+String Trigpair = "dafult";
+String TrigMode = "dafult";
 
-
+const char* param01 = "sendToMac";
+const char* param02 = "receiveFromMac";
+const char* param03 = "onTime";
+const char* param04 = "offTime";
+const char* param05 = "ledPattern";
+const char* param06 = "deviceMode";
+const char* param07 = "pairingToggle";
+const char* param08 = "pairingMode";
 
 
 // Replaces placeholder with LED state value
 String processor(const String& var){
   Serial.println(var);
-  if(var == "DEVICESTATUS"){
-    if(setD == "1"){
-      DEVICE = "MASTER";
-    }
-    else if (setD == "0"){
-      DEVICE = "SLAVE";
-    }
-    Serial.println(DEVICE);
-    return DEVICE;
-  }
-  if(var == "timeON")
+  if(var == "MacSend")
+    return SendMac;
+  if(var == "MacReceive")
+    return ReceiveMac;
+  if(var == "V_on")
     return Ontime;
-  if(var == "timeOFF")
+  if(var == "V_off")
     return Offtime;
   if(var == "Patt")
     return Pattern;
+  if(var == "D_status")
+    return DeviceMode;
+  if(var == "TrigP")
+    return Trigpair;
+  if(var == "TrigM")
+    return TrigMode;
   return String();
 }
     
@@ -92,45 +93,51 @@ void setup(){
   server.begin();
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    request->send(SPIFFS, "/main.html", String(), false, processor);
   });
   
   // Route to load style.css file
   // server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
   //   request->send(SPIFFS, "/style.css", "text/css");
   // });
-  server.on("/submit", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/main.html", HTTP_GET, [](AsyncWebServerRequest *request){
    if(request->hasParam(param01)){
-      setD = request->getParam(param01)->value();
-      Serial.print("setD is ");Serial.println(setD);
+      SendMac = request->getParam(param01)->value();
+      Serial.print("SendMac is ");Serial.println(SendMac);
     }
     if(request->hasParam(param02)){
-      Ontime = request->getParam(param02)->value();
-      Serial.print("ONTIME is ");Serial.println(Ontime);
+      ReceiveMac = request->getParam(param02)->value();
+      Serial.print("ReceiveMac is ");Serial.println(ReceiveMac);
     }
     if(request->hasParam(param03)){
-      Offtime = request->getParam(param03)->value();
-      Serial.print("OFFTIME is ");Serial.println(Offtime);
+      Ontime = request->getParam(param03)->value();
+      Serial.print("Ontime is ");Serial.println(Ontime);
     }
     if(request->hasParam(param04)){
-      Pattern = request->getParam(param04)->value();
+      Offtime = request->getParam(param04)->value();
+      Serial.print("Offtime is ");Serial.println(Offtime);
+    }
+    if(request->hasParam(param05)){
+      Pattern = request->getParam(param05)->value();
       Serial.print("Pattern is ");Serial.println(Pattern);
     }
-    request->send(SPIFFS, "/index.html", String(), false, processor);
+    if(request->hasParam(param06)){
+      DeviceMode = request->getParam(param06)->value();
+      Serial.print("DeviceMode is ");Serial.println(DeviceMode);
+    }
+    if(request->hasParam(param07)){
+      Trigpair = request->getParam(param07)->value();
+      Serial.print("Trigpair is ");Serial.println(Trigpair);
+    }
+    if(request->hasParam(param08)){
+      TrigMode = request->getParam(param08)->value();
+      Serial.print("TrigMode is ");Serial.println(TrigMode);
+    }
+    request->send(SPIFFS, "/main.html", String(), false, processor);
   });
 
 }
  
 void loop(){
-  delay(1000);
-    if (setD == "0"){
-      Serial.print("Device is Slave  "); Serial.println(setD);
-    } else if (setD == "1"){
-      Serial.print("Device is Master  "); Serial.println(setD);
-    }
-    Serial.print("ontime is ");Serial.println(Ontime);
-    Serial.print("offtime is ");Serial.println(Offtime);
-    Serial.print("Pattern is ");Serial.println(Pattern);
-    Serial.println("NEWWWWWW");
-    delay(1000);
+
 }
